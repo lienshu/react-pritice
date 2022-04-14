78d4528b5d98e801084eb5acd965c0c1e7a2a6db
lienshu <1480809371@qq.com>
2022-04-14 11:37:57 +0800

    fix: 联系

--- src/pritice/useeffect.tsx
+++ src/pritice/useeffect.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useEffect, useMemo } from 'react'
 import { useState } from 'react'
 
 // 产品名称列表
@@ -24,6 +24,13 @@ const Example = () => {
     console.log('price变化了')
   }, [price])
 
+  // memo化的name属性
+  const memoName = useMemo(() => {
+    console.log('memoName触发')
+    setPrice(8)
+    return name + 1
+  }, [name])
+
   // 点击修改名字时，打印结果
   // getProductName触发
   // name变化了
@@ -36,11 +43,15 @@ const Example = () => {
 
   // 官网解释：当调用useEffect时，它会在每次渲染后调用，也就是完成对DOM的更改后运行”副作用“函数
   // 当DOM发生变化时，不相关的函数不需要触发，而effect只能在DOM更新后触发，所以需要使用useMemo
+  // 为什么useMemo可以解决，官网解释：传入useMemo的函数会在渲染期间执行
+  // useMemo和useCallback相近，useCallback返回的是一个回调函数，useMomo返回的是一个缓存的函数，数值
   return (
     <Fragment>
       <p>{name}</p>
       <p>{price}</p>
       <p>{getProductName()}</p>
+      {/* 使用useMemo显示的结果 */}
+      <p>{memoName}</p>
       <button onClick={() => setPrice(price + 1)}>价钱+1</button>
       <button onClick={() => setName(nameList[(Math.random() * nameList.length) << 0])}>
         修改名字
